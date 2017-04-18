96aaaffb8922690cbf588b657e365e7cffbd59a3
willemJav <willemfritsdragstra@gmail.com>
2017-04-18 13:48:53 +0200

    Add files via upload

---
+++ latencyobject/test/java/service/TestRandomWordImpl.java
@@ -0,0 +1,18 @@
+package service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRandomWordImpl {
+
+	@Test
+	public void randomWord() {
+		RandomWordImpl rw = new RandomWordImpl();
+		String testString = rw.createWord();
+		int length = testString.length();
+		assertTrue(testString instanceof String && length <= 13 && length != 0 
+				&& (testString.charAt(length-1) == ' '));
+	}
+}
+ 
